fff33b00a91f69b3f11bc6f2c1e4d8af68ef3c3f
joychae <sooie35@gmail.com>
2021-10-17 12:31:34 +0900

    test : add test code for making url

--- src/test/java/com/renewal/weatherservicev2/domain/vo/openapi/HealthAsthmaIdxUrlRequestVOTest.java
+++ src/test/java/com/renewal/weatherservicev2/domain/vo/openapi/HealthAsthmaIdxUrlRequestVOTest.java
@@ -1,12 +1,28 @@
 package com.renewal.weatherservicev2.domain.vo.openapi;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class HealthAsthmaIdxUrlRequestVOTest {
 
     @Test
+    @DisplayName("URL 생성 확인")
     void makeUrl() {
+
+        HealthAsthmaIdxUrlRequestVO request = HealthAsthmaIdxUrlRequestVO.builder()
+                .admCode("1100000000")
+                .date("20210922")
+                .build();
+
+        String result = request.makeUrl().toString();
+        String expected = "http://apis.data.go.kr/1360000/HealthWthrIdxServiceV2/getAsthmaIdxV2?serviceKey=zhvzvF5vNC7ufu7H%2BQnPJtEQbF2QdNZ0qdvZWLeR%2BnL0UwxwnCgrkmxKB9oqCXVSJp95YTliRHwzxvGdrvjetg%3D%3D&areaNo=1100000000&time=2021092206&dataType=json";
+        assertEquals(expected, result);
     }
 }
---
+++ src/test/java/com/renewal/weatherservicev2/service/connection/ConnectionServiceTest.java
@@ -0,0 +1,13 @@
+package com.renewal.weatherservicev2.service.connection;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConnectionServiceTest {
+
+    @Test
+    void connect() {
+
+    }
+}
