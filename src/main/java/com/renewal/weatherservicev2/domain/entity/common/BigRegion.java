fef33d5248514e0a4d4077198102d37691c45b5e
joychae <sooie35@gmail.com>
2021-10-22 01:01:45 +0900

    feat : generate entities

---
+++ src/main/java/com/renewal/weatherservicev2/domain/entity/common/BigRegion.java
@@ -0,0 +1,32 @@
+package com.renewal.weatherservicev2.domain.entity.common;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+public class BigRegion extends BaseTime {
+
+    @Id
+    @Column(name = "big_region_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    String name;
+
+    @Column
+    String admCode;
+
+    @Column
+    String longitude;
+
+    @Column
+    String latitude;
+
+}
---
+++ src/main/java/com/renewal/weatherservicev2/domain/entity/common/SmallRegion.java
@@ -0,0 +1,36 @@
+package com.renewal.weatherservicev2.domain.entity.common;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+public class SmallRegion extends BaseTime {
+
+    @Id
+    @Column(name = "small_region_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "big_region_id")
+    private BigRegion bigRegion;
+
+    @Column
+    String name;
+
+    @Column
+    String admCode;
+
+    @Column
+    String longitude;
+
+    @Column
+    String latitude;
+
+}
